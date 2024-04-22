684d5b14314bccb81ee33a6d4bb696858cee9b07
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-04-22 22:49:14 +0900

    list1-7 1-8 ・コメントはあくまでも、読み手に伝えたい情報を伝えるための機能 ・#だけで、実質的な動きをしないのが、空指令

---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/null_derective.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#
+#define MAX 100
+#
+#define MIN 0
+#
+
+int main() {
+	int a = 0;
+
+	if (a >= MIN) {
+		printf("a‚Ì’l‚Í%d‚Å‚·\n", a);
+	}
+
+	
+}
---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/null_line.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define MAX 100
+
+#define MIN 0
+
+
+int main() {
+	int a = MIN;
+
+	if (a < MAX) {
+		printf("a‚Ì’l‚Í%d‚Å‚·\n", a);
+	}
+
+
+}
