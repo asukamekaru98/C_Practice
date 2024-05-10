619fb7b6ecf59ede963b5ec3d1b2733c44a62384
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-05-10 21:25:12 +0900

    List2-3

---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/chap02/if1a.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int n;
+
+	printf("n‚Ì’l : ");
+	scanf("%d", &n);
+
+	if (n != 0) {
+		puts("0‚¶‚á‚È‚¢");
+	}
+	else {
+		puts("0");
+	}
+}
