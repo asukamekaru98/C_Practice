275074aa7119d10d71cdf66e87088c08e6eb06ad
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-05-17 08:47:44 +0900

    list 3-3

---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/chap03/power.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+double power(double x, int n);
+
+int main()
+{
+	double x;
+	int k, n;
+
+	printf("é¿êîx : ");
+	scanf("%lf", &x);
+
+	printf("é¿êîk : ");
+	scanf("%d", &k);
+
+	printf("é¿êîn : ");
+	scanf("%d", &n);
+
+	printf("xÇÃnèÊ = %f\n", power(x, n));
+	printf("kÇÃnèÊ = %f\n", power(k, n));
+}
+
+double power(double x, int n)
+{
+	double tmp = 1.0;
+
+	for (int i = 1;i <= n; i++) {
+		tmp *= x;
+	}
+	return tmp;
+}
