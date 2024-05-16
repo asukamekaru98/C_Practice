3fc0fbbe434207aa1b220dd940e0cc2735a0c103
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-05-16 19:44:22 +0900

    list3-1

---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/chap02/sqr1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+double sqr(double x)
+{
+	return x * x;
+}
+
+int main()
+{
+	double x;
+	printf("À”’lF");
+	scanf("%lf", &x);
+
+	printf("2æ‚Í%.3f\n", sqr(x));
+
+
+}
