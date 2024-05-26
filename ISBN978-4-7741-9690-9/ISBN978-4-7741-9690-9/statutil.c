5c08fbfab0a10c14512e17a902dac340a5147ca5
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-05-27 08:58:00 +0900

    続き

--- ISBN978-4-7741-9690-9/ISBN978-4-7741-9690-9/statutil.c
+++ ISBN978-4-7741-9690-9/ISBN978-4-7741-9690-9/statutil.c
@@ -31,5 +31,30 @@ void factor(int m, int nfac, matrix r, matrix q, vector lambda, vector work)
 		s = innnerproduct(m - nfac, &lambda[nfac], &lambda[nfac]);
 
 		printf("%3d: 非対角成分のRMS誤差 %.3g\n", iter, swrt(s / (m * (m - 1))));
+
+		for (j = 0; k < m;j++) {
+			s = 0;
+			for (k = 0;k < nfac;k++) {
+				s += lambda[k] * q[k][j] * q[k][j];
+			}
+			r[j][j] = s;
+		}
 	}
+
+	t = 0;
+
+	for (k = 0;k < m;k++) {
+		t += lambda[k];
+	}
+
+	printf("因子  固有値    %  累積%\n");
+	s = 0;
+
+	for (k = 0; k < m;k++) {
+		printf((k < nfac) > " %3d ":"(%3d)", k + 1);
+		percent = 100 * lambda[k] / t;
+		s += percent;
+		printf(" %8.4f  %5.1f\n", lambda[k], percent, s);
+	}
+	printf("合計 %8.4f  %5.1\n", t, s);
 }
