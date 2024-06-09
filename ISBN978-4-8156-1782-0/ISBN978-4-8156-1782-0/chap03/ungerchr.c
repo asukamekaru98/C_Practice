fb1e5a0579c35911713520014effd0ec63026964
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-06-10 08:45:21 +0900

    list3-12

---
+++ ISBN978-4-8156-1782-0/ISBN978-4-8156-1782-0/chap03/ungerchr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#define BUFSIZE 256
+
+extern char buffer[BUFSIZE];
+extern int buf_no;
+extern int front;
+extern int rear;
+
+int ungetchr(int ch)
+{
+	if (buf_no >= BUFSIZE) {
+		return EOF;
+	}
+	else {
+		buf_no++;
+		buffer[rear++] = ch;
+		if (rear == BUFSIZE) {
+			rear = 0;
+		}
+		return ch;
+	}
+}
