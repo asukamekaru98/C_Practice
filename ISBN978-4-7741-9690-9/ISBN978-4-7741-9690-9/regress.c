951ab26da0d5c53bb398c79d9aa6bbf443185dcf
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-06-19 08:41:38 +0900

    回帰分析

---
+++ ISBN978-4-7741-9690-9/ISBN978-4-7741-9690-9/regress.c
@@ -0,0 +1,17 @@
+#include "statutil.c"
+
+#define PIVOTING	0
+#define VERBOSE		0
+#define EPS			1e-6
+
+#if ! PIVOTING
+
+int lsq(int n, int m, matrix x, vector b, int* col, vector normsq)
+{
+
+}
+
+#else
+
+
+#endif
