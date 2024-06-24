8dba45bbfd973ecfe0f35dede3e9c45a789d10a0
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-06-24 13:02:43 +0900

    続き

--- ISBN978-4-7741-9690-9/ISBN978-4-7741-9690-9/regress.c
+++ ISBN978-4-7741-9690-9/ISBN978-4-7741-9690-9/regress.c
@@ -21,6 +21,23 @@ int lsq(int n, int m, matrix x, vector b, int* col, vector normsq)
 	for (k = 0; k < m;k++) {
 		if (normsq[k] == 0)continue;
 
+		v = x[k];
+		u = innerproduct(n - r, &v[r], &v[r]);
+#if VERBOSE
+		printf("\n%4d: 2æ˜a+‰Šú2æ˜a = %-14g", k + 1, u / normsq[k]);
+#endif
+
+		if (u / normsq[k] < ESP * ESP) {
+			continue;
+		}
+
+		x[r] = v;
+		col[r] = k;
+		u = sqrt(u);
+		if (v[r] < 0) {
+			u = -u;
+		}
+
 	}
 
 }
